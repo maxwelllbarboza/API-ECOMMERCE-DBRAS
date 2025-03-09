37312a334e012f057b4fc647ada86b3153f9c996
Maxwell <Maxwellbarboza@hotmail.com>
2025-03-09 12:43:49 -0300

    Alterei o script de build

--- .history/src/main_20250309122253.ts
+++ .history/src/main_20250309122253.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from 'process';
+import * as process from 'pr'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
